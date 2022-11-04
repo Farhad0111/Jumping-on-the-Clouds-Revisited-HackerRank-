51d380884dd5c806878caa6dd191ff60ffba753a
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2022-11-04 20:47:24 +0600

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,52 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+*******************************************************************************/
+
+#include <iostream>
+#include<vector>
+using namespace std;
+class Farhad{
+public:    
+  int n,k;
+  int c[10005];
+  //vector<int>c(10005);
+  public:
+    int faru(){
+        //int n,k;
+        cin>>n>>k;
+        //vector<int>c(n);
+        for(int i=0;i<n;i++){
+            cin>>c[i];
+        }
+        return 0;
+    }
+};
+class Farhad1:public Farhad{
+  public:
+    int e=100,cur=0;
+    public:
+    int display(){
+        faru();
+        while(true){
+            cur+=k;
+            cur%=n;
+            if(c[cur]){
+                e-=2;
+            }
+            e--;
+            if(cur==0){
+                break;
+            }
+        }
+        cout<<e<<endl;
+        return 0;
+    }
+};
+int main(){
+    Farhad1 farhad=Farhad1();
+    farhad.display();
+    return 0;
+}
